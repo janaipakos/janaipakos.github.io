a412b4c6cc42ee703216340d90c86daff5417c28
janaipakos <janaipakos@gmail.com>
2015-06-02 00:39:06 -0400

    edit

--- justin/justin.js
+++ justin/justin.js
@@ -3,11 +3,11 @@ confirm("I am ready to play!");
 var age=prompt("What's your age");
 if(age < 13)
 {
-    console.log("You are allowed to play, but we take no responsibility");
+    alert("You are allowed to play, but we take no responsibility");
 } 
 else
 {
-    console.log("Have a fun game");
+    alert("Have a fun game");
 }
 console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'");
 console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'");
